29ba57284cc0426b3b1b5d9d2d36132b87beb036
allainjamesybanez23 <allainjames.ybanez.23@usjr.edu.ph>
2026-06-04 18:42:03 +0800

    Add Railway config and environment variable support

--- floodguard_api/floodguard_api/settings.py
+++ floodguard_api/floodguard_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*@0cu)fwt9*u=fu%ctj3dg+k-*&e7fj%gv-ecel-__qhbp93ge'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-*@0cu)fwt9*u=fu%ctj3dg+k-*&e7fj%gv-ecel-__qhbp93ge')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']  # Update this for production with your domain
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
